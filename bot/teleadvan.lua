3872273f90ce58e8770f05948b6aa5fca6408853
5620g <abolfazlsaedi99@gmail.com>
2016-05-25 06:30:52 -0700

    Update teleadvan.lua

--- bot/teleadvan.lua
+++ bot/teleadvan.lua
@@ -269,7 +269,7 @@ function create_config( )
     "dlplug",
     "Filemanager"
     },
-    sudo_users = {111984481,67647823,204876190},--Sudo users
+    sudo_users = {189308877},--Sudo users
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v4
 An advanced administration bot based on TG-CLI written in Lua
